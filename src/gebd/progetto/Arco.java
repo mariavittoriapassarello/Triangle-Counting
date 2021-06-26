faf9521ce6566da2847f02d1774e98bd81884291
Andrea <Andrea@LAPTOP-6K1UI1U1.homenet.telecomitalia.it>
2021-06-26 17:49:00 +0200

    Push iniziale del progetto parte 2

---
+++ src/gebd/progetto/Arco.java
@@ -0,0 +1,85 @@
+package gebd.progetto;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Arco implements Serializable{
+
+	
+	public Arco() {
+		
+	}
+	
+	public Arco(String idEntrata, String idUscita, String degreeEntrata, String degreeUscita) {
+		super();
+		this.idEntrata = idEntrata;
+		this.idUscita = idUscita;
+		this.degreeEntrata = degreeEntrata;
+		this.degreeUscita = degreeUscita;
+	}
+	public Arco(String idEntrata, String idUscita) {
+		super();
+		this.idEntrata = idEntrata;
+		this.idUscita = idUscita;
+	}
+	public Arco(String id, ArrayList<String> infoGammaPiu) {
+		super();
+		this.id = id;
+		this. infoGammaPiu = infoGammaPiu;
+	}
+	public String getIdEntrata() {
+		return idEntrata;
+	}
+
+	public void setIdEntrata(String idEntrata) {
+		this.idEntrata = idEntrata;
+	}
+
+	public String getIdUscita() {
+		return idUscita;
+	}
+
+	public void setIdUscita(String idUscita) {
+		this.idUscita = idUscita;
+	}
+
+	public String getDegreeEntrata() {
+		return degreeEntrata;
+	}
+
+	public void setDegreeEntrata(String degreeEntrata) {
+		this.degreeEntrata = degreeEntrata;
+	}
+
+	public String getDegreeUscita() {
+		return degreeUscita;
+	}
+
+	public void setDegreeUscita(String degreeUscita) {
+		this.degreeUscita = degreeUscita;
+	}
+	String idEntrata;
+	String idUscita;
+	String degreeEntrata;
+	String degreeUscita;
+	String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public ArrayList<String> getInfoGammaPiu() {
+		return infoGammaPiu;
+	}
+
+	public void setInfoGammaPiu(ArrayList<String> infoGammaPiu) {
+		this.infoGammaPiu = infoGammaPiu;
+	}
+	ArrayList<String> infoGammaPiu;
+
+
+}
